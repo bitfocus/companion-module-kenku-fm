2eb0553794cf796fd2fd18c8fc3eed699cbde9a0
Schemen <schemen@users.noreply.github.com>
2023-11-29 23:55:13 +0100

    Move Statefunctions to state.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const UpgradeScripts = require('./upgrades')
 const UpdateActions = require('./actions')
 const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
+const stateFunctions = require('./state');
 const http = require('http');
 
 
@@ -31,14 +32,13 @@ class ModuleInstance extends InstanceBase {
 		this.SoundboardDataState = {
 			playlists: [],
 			tracks: []
-		  };
+		};
 
 		this.pollingInterval = null;
 	}
 
 	async init(config) {
 		this.config = config
-
 		this.updateStatus(InstanceStatus.Ok)
 
 		this.updateActions() // export actions
@@ -47,8 +47,8 @@ class ModuleInstance extends InstanceBase {
 
 
 		// Get initial state
-		this.updatePlaylistPlaybackState()
-		this.updateSoundboardPlaybackState()
+		stateFunctions.updatePlaylistPlaybackState(this)
+		stateFunctions.updateSoundboardPlaybackState(this)
 		this.generatePresets()
 
 		// Start Polling
@@ -100,6 +100,7 @@ class ModuleInstance extends InstanceBase {
 		UpdateVariableDefinitions(this)
 	}
 
+
 	startPresetsPolling() {
 		const presetsPollIntervalMs = 30000; // For example, poll every 30 seconds for presets
 	  
@@ -132,8 +133,8 @@ class ModuleInstance extends InstanceBase {
 		};
 	  
 		this.pollingInterval = setInterval(() => {
-		  poll(this.updatePlaylistPlaybackState.bind(this));
-		  poll(this.updateSoundboardPlaybackState.bind(this));
+			poll(() => stateFunctions.updatePlaylistPlaybackState(this));
+			poll(() => stateFunctions.updateSoundboardPlaybackState(this));
 		}, pollIntervalMs);
 	}
 
@@ -147,7 +148,7 @@ class ModuleInstance extends InstanceBase {
 	async generatePresets() {
 		const presets = {};
 		this.log('debug', "Generating Presets")
-		await this.fetchPlaylistData()
+		await stateFunctions.fetchPlaylistData(this)
 		this.PlaylistDataState.playlists?.forEach((playlist, index) => {
 		  presets[`playlist_${index}`] = {
 			type: 'button',
@@ -187,7 +188,7 @@ class ModuleInstance extends InstanceBase {
 			],
 		  };
 		});
-		await this.fetchSoundboardData();
+		await stateFunctions.fetchSoundboardData(this);
 		this.SoundboardDataState.sounds?.forEach((sound, index) => {
 		  presets[`sound_${index}`] = {
 			type: 'button',
@@ -229,186 +230,6 @@ class ModuleInstance extends InstanceBase {
 	  
 		this.setPresetDefinitions(presets);
 	}	
-
-	// Populate PlaylistDataState
-	async fetchPlaylistData() {
-		const options = {
-		  hostname: this.config.host,
-		  port: this.config.port,
-		  path: '/v1/playlist',
-		  method: 'GET',
-		};
-	  
-		return new Promise((resolve, reject) => {
-		  const req = http.request(options, (res) => {
-			let data = '';
-	  
-			res.on('data', (chunk) => {
-			  data += chunk;
-			});
-			res.on('end', () => {
-			  try {
-				const parsedData = JSON.parse(data);
-				// Update the PlaylistDataState with the received data
-				this.PlaylistDataState.playlists = parsedData.playlists;
-				this.PlaylistDataState.tracks = parsedData.tracks;
-				resolve();
-			  } catch (e) {
-				this.log('error', 'Failed to parse playlist data: ' + e.message);
-				reject(e);
-			  }
-			});
-		  });
-	  
-		  req.on('error', (e) => {
-			this.log('error', 'Failed to get playlist data: ' + e.message);
-			reject(e);
-		  });
-	  
-		  req.end();
-		});
-	}	
-
-	// Populate SoundboardDataState
-	async fetchSoundboardData() {
-		const options = {
-		  hostname: this.config.host,
-		  port: this.config.port,
-		  path: '/v1/soundboard',
-		  method: 'GET',
-		};
-	  
-		return new Promise((resolve, reject) => {
-		  const req = http.request(options, (res) => {
-			let data = '';
-	  
-			res.on('data', (chunk) => {
-			  data += chunk;
-			});
-			res.on('end', () => {
-			  try {
-				const parsedData = JSON.parse(data);
-				// Update the PlaylistDataState with the received data
-				this.SoundboardDataState.soundboards = parsedData.soundboards;
-				this.SoundboardDataState.sounds = parsedData.sounds;
-				resolve();
-			  } catch (e) {
-				this.log('error', 'Failed to parse soundboard data: ' + e.message);
-				reject(e);
-			  }
-			});
-		  });
-	  
-		  req.on('error', (e) => {
-			this.log('error', 'Failed to get soundboard data: ' + e.message);
-			reject(e);
-		  });
-	  
-		  req.end();
-		});
-	}
-
-	async updatePlaylistPlaybackState() {
-		const options = {
-		  hostname: this.config.host,
-		  port: this.config.port,
-		  path: '/v1/playlist/playback',
-		  method: 'GET',
-		};
-	  
-		return new Promise((resolve, reject) => {
-		  const req = http.request(options, (res) => {
-			let data = '';
-			
-			res.on('data', (chunk) => {
-			  data += chunk;
-			});
-			res.on('end', () => {
-			  try {
-				const parsedData = JSON.parse(data);
-				// Updating the PlaylistPlaybackState with the received data
-				this.PlaylistPlaybackState.playing = parsedData.playing;
-				this.PlaylistPlaybackState.volume = parsedData.volume;
-				this.PlaylistPlaybackState.muted = parsedData.muted;
-				this.PlaylistPlaybackState.shuffle = parsedData.shuffle;
-				this.PlaylistPlaybackState.repeat = parsedData.repeat;
-
-				// For optional properties like 'track' and 'playlist', check if they exist before updating
-				this.PlaylistPlaybackState.track = parsedData.track ? {
-				id: parsedData.track.id,
-				url: parsedData.track.url,
-				title: parsedData.track.title,
-				duration: parsedData.track.duration,
-				progress: parsedData.track.progress,
-				} : null;
-
-				this.PlaylistPlaybackState.playlist = parsedData.playlist ? {
-				id: parsedData.playlist.id,
-				title: parsedData.playlist.title,
-				} : null;
-
-				// Update variables
-				this.updateVariableValues();
-
-				resolve(this.PlaylistPlaybackState); // Resolve the promise with the updated state
-				} catch (e) {
-				this.log('error', 'Failed to parse playlist playback state: ' + e.message);
-				reject(e); // Reject the promise if there's an error
-				}
-			});
-		  });
-	  
-		  req.on('error', (e) => {
-			this.log('error', 'Failed to get playlist playback state: ' + e.message);
-			reject(e); // Reject the promise on request error
-		  });
-	  
-		req.end();
-		this.log('debug', `Updated PlaylistPlaybackState`);
-		});
-	  }
-
-
-	async updateSoundboardPlaybackState() {
-		const options = {
-			hostname: this.config.host,
-			port: this.config.port,
-			path: '/v1/soundboard/playback',
-			method: 'GET',
-		};
-
-		return new Promise((resolve, reject) => {
-			const req = http.request(options, (res) => {
-			let data = '';
-
-			res.on('data', (chunk) => {
-				data += chunk;
-			});
-			res.on('end', () => {
-				try {
-				const parsedData = JSON.parse(data);
-				// Updating the SoundboardPlaybackState with the received data
-				this.SoundboardPlaybackState.sounds = parsedData.sounds;
-
-				// Update variables & feedbacks
-				this.updateVariableValues();
-
-				resolve(this.SoundboardPlaybackState); // Resolve with the updated state
-				} catch (e) {
-				this.log('error', 'Failed to parse soundboard playback state: ' + e.message);
-				reject(e);
-				}
-			});
-			});
-
-			req.on('error', (e) => {
-			this.log('error', 'Failed to get soundboard playback state: ' + e.message);
-			reject(e);
-			});
-			req.end();
-			this.log('debug', `Updated SoundboardPlaybackState`);
-		});
-	}	  
 
 	updateVariableValues() {
 	this.checkFeedbacks('playlist_playback_feedback', 'playlist_shuffle_feedback', 'playlist_repeat_feedback', 'playlist_feedback', 'playlist_mute_feedback', 'soundboard_sound_feedback');
---
+++ state.js
@@ -0,0 +1,189 @@
+const http = require('http');
+
+// Populate PlaylistDataState
+async function fetchPlaylistData(self) {
+    const options = {
+        hostname: self.config.host,
+        port: self.config.port,
+        path: '/v1/playlist',
+        method: 'GET',
+    };
+    
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (res) => {
+        let data = '';
+    
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            try {
+            const parsedData = JSON.parse(data);
+            // Update the PlaylistDataState with the received data
+            self.PlaylistDataState.playlists = parsedData.playlists;
+            self.PlaylistDataState.tracks = parsedData.tracks;
+            resolve();
+            } catch (e) {
+            self.log('error', 'Failed to parse playlist data: ' + e.message);
+            reject(e);
+            }
+        });
+        });
+    
+        req.on('error', (e) => {
+        self.log('error', 'Failed to get playlist data: ' + e.message);
+        reject(e);
+        });
+    
+        req.end();
+    });
+}	
+
+// Populate SoundboardDataState
+async function fetchSoundboardData(self) {
+    const options = {
+        hostname: self.config.host,
+        port: self.config.port,
+        path: '/v1/soundboard',
+        method: 'GET',
+    };
+    
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (res) => {
+        let data = '';
+    
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            try {
+            const parsedData = JSON.parse(data);
+            // Update the PlaylistDataState with the received data
+            self.SoundboardDataState.soundboards = parsedData.soundboards;
+            self.SoundboardDataState.sounds = parsedData.sounds;
+            resolve();
+            } catch (e) {
+            self.log('error', 'Failed to parse soundboard data: ' + e.message);
+            reject(e);
+            }
+        });
+        });
+    
+        req.on('error', (e) => {
+        self.log('error', 'Failed to get soundboard data: ' + e.message);
+        reject(e);
+        });
+    
+        req.end();
+    });
+}
+
+// Populate PlaylistPlaybackState
+async function updatePlaylistPlaybackState(self) {
+    const options = {
+        hostname: self.config.host,
+        port: self.config.port,
+        path: '/v1/playlist/playback',
+        method: 'GET',
+    };
+    
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (res) => {
+        let data = '';
+        
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            try {
+            const parsedData = JSON.parse(data);
+            // Updating the PlaylistPlaybackState with the received data
+            self.PlaylistPlaybackState.playing = parsedData.playing;
+            self.PlaylistPlaybackState.volume = parsedData.volume;
+            self.PlaylistPlaybackState.muted = parsedData.muted;
+            self.PlaylistPlaybackState.shuffle = parsedData.shuffle;
+            self.PlaylistPlaybackState.repeat = parsedData.repeat;
+
+            // For optional properties like 'track' and 'playlist', check if they exist before updating
+            self.PlaylistPlaybackState.track = parsedData.track ? {
+            id: parsedData.track.id,
+            url: parsedData.track.url,
+            title: parsedData.track.title,
+            duration: parsedData.track.duration,
+            progress: parsedData.track.progress,
+            } : null;
+
+            self.PlaylistPlaybackState.playlist = parsedData.playlist ? {
+            id: parsedData.playlist.id,
+            title: parsedData.playlist.title,
+            } : null;
+
+            // Update variables
+            self.updateVariableValues();
+
+            resolve(self.PlaylistPlaybackState); // Resolve the promise with the updated state
+            } catch (e) {
+            self.log('error', 'Failed to parse playlist playback state: ' + e.message);
+            reject(e); // Reject the promise if there's an error
+            }
+        });
+        });
+    
+        req.on('error', (e) => {
+        self.log('error', 'Failed to get playlist playback state: ' + e.message);
+        reject(e); // Reject the promise on request error
+        });
+    
+    req.end();
+    self.log('debug', `Updated PlaylistPlaybackState`);
+    });
+}
+
+// Populate SoundboardPlaybackState
+async function updateSoundboardPlaybackState(self) {
+    const options = {
+        hostname: self.config.host,
+        port: self.config.port,
+        path: '/v1/soundboard/playback',
+        method: 'GET',
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            try {
+            const parsedData = JSON.parse(data);
+            // Updating the SoundboardPlaybackState with the received data
+            self.SoundboardPlaybackState.sounds = parsedData.sounds;
+
+            // Update variables & feedbacks
+            self.updateVariableValues();
+
+            resolve(self.SoundboardPlaybackState); // Resolve with the updated state
+            } catch (e) {
+            self.log('error', 'Failed to parse soundboard playback state: ' + e.message);
+            reject(e);
+            }
+        });
+        });
+
+        req.on('error', (e) => {
+        self.log('error', 'Failed to get soundboard playback state: ' + e.message);
+        reject(e);
+        });
+        req.end();
+        self.log('debug', `Updated SoundboardPlaybackState`);
+    });
+}	  
+
+module.exports = {
+    updatePlaylistPlaybackState,
+    updateSoundboardPlaybackState,
+    fetchSoundboardData,
+    fetchPlaylistData,
+};
